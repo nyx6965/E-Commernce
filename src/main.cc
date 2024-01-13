26e4c5fac77c479bae98493c963728df593256b1
amr8644 <72759681+amr8644@users.noreply.github.com>
2024-01-13 21:17:34 +0300

    Added some

--- src/main.cc
+++ src/main.cc
@@ -14,8 +14,43 @@ int main(void) {
 
   SetTargetFPS(60); // Set our game to run at 60 frames-per-second
 
+  // Custom timming variables
+  double previousTime = GetTime(); // Previous time measure
+  double currentTime = 0.0;        // Current time measure
+  double updateDrawTime = 0.0;     // Update + Draw time
+  double waitTime = 0.0;           // Wait time (if target fps required)
+  float deltaTime = 0.0f;          // Frame time (Update + Draw + Wait time)
+
+  float timeCounter = 0.0f; // Accumulative time counter (seconds)
+  float position = 0.0f;    // Circle position
+  bool pause = false;       // Pause control flag
+
+  int targetFPS = 60; // Our initial target fps
+  //--------------------------------------------------------------------------------------
   while (!WindowShouldClose()) // Detect window close button or ESC key
   {
+
+    // Update
+    //----------------------------------------------------------------------------------
+    PollInputEvents(); // Poll input events (SUPPORT_CUSTOM_FRAME_CONTROL)
+
+    if (IsKeyPressed(KEY_SPACE))
+      pause = !pause;
+
+    if (IsKeyPressed(KEY_UP))
+      targetFPS += 20;
+    else if (IsKeyPressed(KEY_DOWN))
+      targetFPS -= 20;
+
+    if (targetFPS < 0)
+      targetFPS = 0;
+
+    if (!pause) {
+      position += 200 * deltaTime; // We move at 200 pixels per second
+      if (position >= GetScreenWidth())
+        position = 0;
+      timeCounter += deltaTime; // We count time (seconds)
+    }
 
     BeginDrawing();
 
@@ -38,7 +73,9 @@ int main(void) {
     // Get the mouse position
     Vector2 mousePosition = GetMousePosition();
 
-    DrawCircleV(mousePosition, 1.5, BLACK);
+
+     DrawCircle((int)position, GetScreenHeight() / 2 - 25, 50, RED);
+    //DrawCircleV(mousePosition, 1.5, BLACK);
 
     EndDrawing();
   }
