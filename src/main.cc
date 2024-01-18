ffc3845dbebcc0b13371f07b2c437a5a2c654258
amr8644 <72759681+amr8644@users.noreply.github.com>
2024-01-19 00:53:09 +0300

    Added some

--- src/main.cc
+++ src/main.cc
@@ -12,70 +12,37 @@ int main(void) {
 
   Vector2 touchPositions[MAX_TOUCH_POINTS] = {0};
 
-  SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+  SetTargetFPS(60);
 
-  // Custom timming variables
-  double previousTime = GetTime(); // Previous time measure
-  double currentTime = 0.0;        // Current time measure
-  double updateDrawTime = 0.0;     // Update + Draw time
-  double waitTime = 0.0;           // Wait time (if target fps required)
-  float deltaTime = 0.0f;          // Frame time (Update + Draw + Wait time)
+  for (int i = 0; i < 10; ++i) {
+    touchPositions[i].x = (int)GetRandomValue(0, screenWidth);
+    touchPositions[i].y = (int)GetRandomValue(0, screenHeight);
+  }
 
-  float timeCounter = 0.0f; // Accumulative time counter (seconds)
-  float position = 0.0f;    // Circle position
-  bool pause = false;       // Pause control flag
+  Vector2 current_positon = GetMousePosition();
+  while (!WindowShouldClose()) {
 
-  int targetFPS = 60; // Our initial target fps
-  //--------------------------------------------------------------------------------------
-  while (!WindowShouldClose()) // Detect window close button or ESC key
-  {
+    Vector2 new_pos;
+    new_pos.x = GetMousePosition().x - current_positon.x;
+    new_pos.y = GetMousePosition().y - current_positon.y;
 
-    // Update
-    //----------------------------------------------------------------------------------
-    PollInputEvents(); // Poll input events (SUPPORT_CUSTOM_FRAME_CONTROL)
-
-    if (IsKeyPressed(KEY_SPACE))
-      pause = !pause;
-
-    if (IsKeyPressed(KEY_UP))
-      targetFPS += 20;
-    else if (IsKeyPressed(KEY_DOWN))
-      targetFPS -= 20;
-
-    if (targetFPS < 0)
-      targetFPS = 0;
-
-    if (!pause) {
-      position += 200 * deltaTime; // We move at 200 pixels per second
-      if (position >= GetScreenWidth())
-        position = 0;
-      timeCounter += deltaTime; // We count time (seconds)
+    for (int i = 0; i < 10; ++i) {
+      touchPositions[i].x += new_pos.x;
+      touchPositions[i].y += new_pos.y;
     }
 
     BeginDrawing();
 
-    /*
-    for (int i = 0; i < 2000; ++i){
-      touchPositions[i].x = (int)GetRandomValue(0, screenWidth);
-      touchPositions[i].y = (int)GetRandomValue(0, screenHeight);
-    }
-    */
-
     ClearBackground(RAYWHITE);
 
-    /*
-    for (int i = 0; i < 2000; ++i) {
+    for (int i = 0; i < 10; ++i) {
       if ((touchPositions[i].x > 0) && (touchPositions[i].y > 0)) {
-        DrawCircleV(touchPositions[i], 1.5,BLACK );
+        DrawCircleV(touchPositions[i], 10, BLACK);
+        // DrawCircleV(mousePosition, 1.5, BLACK);
       }
     }
-    */
-    // Get the mouse position
-    Vector2 mousePosition = GetMousePosition();
 
-
-     DrawCircle((int)position, GetScreenHeight() / 2 - 25, 50, RED);
-    //DrawCircleV(mousePosition, 1.5, BLACK);
+    // DrawCircle((int)position, GetScreenHeight() / 2 - 25, 50, RED);
 
     EndDrawing();
   }
