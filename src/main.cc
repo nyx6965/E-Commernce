2f13c6f42b6f38af2c9ba26a9f23a5b302607404
amr8644 <72759681+amr8644@users.noreply.github.com>
2024-02-29 19:25:27 +0300

    fix: Fixed movement issues

--- src/main.cc
+++ src/main.cc
@@ -1,56 +1,71 @@
 #include "raylib.h"
-#include <deque>
+#include <iostream>
+#include <vector>
 
 #define MAX_TOUCH_POINTS 10000
 
+const int screenWidth = 400;
+const int screenHeight = 600;
+
 int main(void) {
-
-  std::deque<Vector2> d;
-
-  const int screenWidth = 400;
-  const int screenHeight = 600;
 
   InitWindow(screenWidth, screenHeight,
              "raylib [core] example - input multitouch");
 
   Vector2 touchPositions[MAX_TOUCH_POINTS] = {0};
 
+  int randValuex = GetRandomValue(0, screenWidth);
+  int randValuey = GetRandomValue(0, screenHeight);
+
   Vector2 test;
+  test.x = randValuex;
+  test.y = randValuey;
+  std::vector<Vector2> stack;
   SetTargetFPS(60);
 
-  /*
-  for (int i = 0; i < MAX_TOUCH_POINTS; ++i) {
-    touchPositions[i].x = (int)GetRandomValue(0, screenWidth);
-    touchPositions[i].y = (int)GetRandomValue(0, screenHeight);
-  }
-  */
-
-  float smo = 0.001;
+  float smo = 0.00001;
 
   while (!WindowShouldClose()) {
+    stack.push_back(GetMousePosition());
 
-    Vector2 mouse_pos = GetMousePosition();
-    test = mouse_pos;
-    d.push_back(mouse_pos);
+    for (auto s : stack) {
+      if (test.x >= GetMousePosition().x) {
+        test.x -= s.x * smo;
+      } else {
+        test.x += s.x * smo;
+      }
+
+      if (test.y >= GetMousePosition().y) {
+        test.y -= s.y * smo;
+      } else {
+        test.y += s.y * smo;
+      }
+    }
+
+
+    if (test.y > screenHeight) {
+      test.y = screenHeight - 10;
+    }
+
+    if (test.x > screenWidth) {
+      test.x = screenWidth - 10;
+    }
+    if (test.x < 0) {
+      test.x = 0;
+    }
+
+    if (test.y < 0) {
+      test.y = 0;
+    }
+
     BeginDrawing();
     ClearBackground(RAYWHITE);
 
-    /*
-     for (int i = 0; i < MAX_TOUCH_POINTS; ++i) {
-
-       }
-
-      */
-    if ((test.x > 0) && (test.y > 0)) {
-      // DrawCircleV(mousePosition, 1.5, BLACK);
-    }
-
-      DrawCircleV(test, 5, BLACK);
-
-    // DrawCircle((int)position, GetScreenHeight() / 2 - 25, 50, RED);
+    DrawCircleV(test, 5, BLACK);
 
     EndDrawing();
   }
+
   CloseWindow(); // Close window and OpenGL context
   return 0;
 }
